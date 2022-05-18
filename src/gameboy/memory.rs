15fc9ab929df3ade28572cedd4095626649ef7a8
Eric Sharkey <ers16e@my.fsu.edu>
2022-05-18 00:47:49 -0400

    Added consts for memory locations

--- src/gameboy/memory.rs
+++ src/gameboy/memory.rs
@@ -1,5 +1,33 @@
 const MEMORY_SIZE: usize = 0xFFFF;
 const BOOT_LOCATION: usize = 0;
+const ROM_BANK_0_LOCATION: (usize,usize) = (0x0000,0x3FFF);
+const ROM_BANK_1_LOCATION: (usize,usize) = (0x4000,0x7FFF);
+const VRAM_LOCATION: (usize, usize) = (0x8000,0x9FFF);
+const EXTERNAL_RAM_LOCATION: (usize,usize) = (0xA000,0xBFFF);
+const WRAM1_LOCATION:(usize,usize) = (0xC000,0xCFFF);
+const WRAM2_LOCATION:(usize,usize) = (0xD000,0xDFFF);
+const ECHO_RAM_LOCATION:(usize,usize) = (0xC000,0xDDFF);
+const SPRITE_TABLE_LOCATION:(usize,usize) = (0xFE00,0xFE9F);
+const NOT_USABLE_LOCATION:(usize,usize) = (0xFEA0,0xFEFF);
+const IO_REGISTERS_LOCATION:(usize,usize) = (0xFF00,0xFF7F);
+const HRAM_LOCATION:(usize,usize) = (0xFF80,0xFFFE);
+const IE_LOCATION:(usize,usize) = (0xFFFF,0xFFFF); 
+
+pub enum MemoryLocation {
+    RomBank0,
+    RomBank1,
+    VideoRAM,
+    ExternalRAM,
+    WorkRAM1,
+    WorkRAM2,
+    EchoRam,
+    SpriteTable,
+    NotUsable,
+    IORegisters,
+    HighRam,
+    InterruptEnableRegister
+}
+
 pub struct Memory {
     bytes: [u8; MEMORY_SIZE]
 }
@@ -10,6 +38,8 @@ impl Memory {
             bytes: [0;MEMORY_SIZE]
         }
     }
+
+
 
     pub fn load_boot_rom(&mut self,data: & Vec<u8>) {
         for (i,elem) in data.iter().enumerate() {
